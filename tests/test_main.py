6070be3d7fae4ea44a361297489b0494f3572638
Eugene Che <36330890+Hardbow@users.noreply.github.com>
2025-07-16 09:58:02 +0300

    parametrize tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,34 +1,33 @@
 import pytest
 from src.main import Calculator
+from contextlib import nullcontext as does_not_raise
 
 
-@pytest.mark.parametrize(
-    "x, y, res",
-    [
-        (1, 2, 0.5),
-        (5, -1, -5),
+class TestCalculator:
+    @pytest.mark.parametrize(
+        "x, y, res, expectation",
+        [
+            (1, 2, 0.5, does_not_raise()),
+            (5, -1, -5, does_not_raise()),
+            (5, "-1", -5, pytest.raises(TypeError)),
+            (5, 0, -5, pytest.raises(ZeroDivisionError)),
+        ]
+    )
+    def test_divide(self, x, y, res, expectation):
+        with expectation:
+            assert Calculator().divide(x, y) == res
 
-
-    ]
- )
-def test_divide(x, y, res):
-    x = 1
-    y = 2
-    res = 0.5
-    assert Calculator().divide(x, y) == res
-
-
-
-@pytest.mark.parametrize(
-    "x, y, res",
-    [
-        (1, 2, 3),
-        (-1, 1, 0),
-        (0.5, 0.5, 1)
-    ]
-)
-def test_add(x, y, res):
-    assert Calculator().add(x, y) == res
+    @pytest.mark.parametrize(
+        "x, y, res",
+        [
+            (1, 2, 3),
+            (-1, 1, 0),
+            (0.5, "0.5", 1)
+        ]
+    )
+    def test_add(self, x, y, res):
+        with pytest.raises(TypeError):
+            assert Calculator().add(x, y) == res
 
 # def test_2():
 #     assert 2 == 2
